9f39b254b1e3947f4e9f34ceb98d4545bdc5745e
ektagupta123 <riyaektagupta61509@gmail.com>
2025-07-09 12:26:20 +1200

    chore: initial backend setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Recipea backend!');
+});
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
